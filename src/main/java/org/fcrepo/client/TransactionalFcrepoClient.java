4f1c6522de6a3e9aecda0ecfcb5761c6ef210109
Michael Ritter <emericaskate15@gmail.com>
2022-07-28 13:23:16 -0600

    Add TransactionalFcrepoClient for automatically adding Atomic-IDs to requests

---
+++ src/main/java/org/fcrepo/client/TransactionalFcrepoClient.java
@@ -0,0 +1,83 @@
+/*
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree.
+ */
+package org.fcrepo.client;
+
+import java.net.URI;
+
+import org.apache.http.impl.client.CloseableHttpClient;
+
+/**
+ * A Transaction aware client which adds the Atomic_ID header to requests
+ *
+ * @author mikejritter
+ */
+public class TransactionalFcrepoClient extends FcrepoClient {
+
+    private final FcrepoResponse.TransactionURI transactionURI;
+
+    /**
+     *
+     * @param transactionURI the transaction to append to all requests
+     * @param httpClient the httpclient
+     * @param throwExceptionOnFailure whether to throw an exception on any non-2xx or 3xx HTTP responses
+     */
+    public TransactionalFcrepoClient(final FcrepoResponse.TransactionURI transactionURI,
+                                     final CloseableHttpClient httpClient,
+                                     final Boolean throwExceptionOnFailure) {
+        super(httpClient, throwExceptionOnFailure);
+        this.transactionURI = transactionURI;
+    }
+
+    @Override
+    public GetBuilder get(final URI url) {
+        final var builder = super.get(url);
+        builder.addTransaction(transactionURI);
+        return builder;
+    }
+
+    @Override
+    public HeadBuilder head(final URI url) {
+        final var builder = super.head(url);
+        builder.addTransaction(transactionURI);
+        return builder;
+    }
+
+    @Override
+    public DeleteBuilder delete(final URI url) {
+        final var builder = super.delete(url);
+        builder.addTransaction(transactionURI);
+        return builder;
+    }
+
+    @Override
+    public OptionsBuilder options(final URI url) {
+        final var builder = super.options(url);
+        builder.addTransaction(transactionURI);
+        return builder;
+    }
+
+    @Override
+    public PatchBuilder patch(final URI url) {
+        final var builder = super.patch(url);
+        builder.addTransaction(transactionURI);
+        return builder;
+    }
+
+    @Override
+    public PostBuilder post(final URI url) {
+        final var builder = super.post(url);
+        builder.addTransaction(transactionURI);
+        return builder;
+    }
+
+    @Override
+    public PutBuilder put(final URI url) {
+        final var builder = super.put(url);
+        builder.addTransaction(transactionURI);
+        return builder;
+    }
+
+}
